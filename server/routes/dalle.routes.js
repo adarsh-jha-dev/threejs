e16e60da10fb4d4591afb98529d62724ed59647e
Adarsh Jha <adarshjhaxif@gmail.com>
2024-01-07 18:12:02 +0530

    Problems in DALL-E image generation, other things done

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,38 +1,30 @@
 import express from "express";
 import * as dotenv from "dotenv";
-
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(config);
-
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "Hello from the dalle route" });
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
 });
+
+const openai = new OpenAI();
+openai.apiKey = process.env.OPENAI_API_KEY;
 
 router.route("/").post(async (req, res) => {
+  const { prompt } = req.body;
   try {
-    const { prompt } = req.body;
-    const response = await openai.createImage({
+    const image = await openai.images.generate({
       prompt,
       n: 1,
-      size: "1024x1024",
       response_format: "b64_json",
+      size: "1024x1024",
     });
-
-    const image = response.data.data[0].b64_json;
-
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(200).json({ photo: image.data.data[0].b64_json });
+  } catch (err) {
+    res.send(err);
   }
 });
 
